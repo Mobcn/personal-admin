5c38fa815bbb6022f852dcc3025aa9d67a9c32f7
Mobcn <mobingcn@gmail.com>
2023-10-21 15:57:45 +0800

    初始化

--- src/declare/auto-import.d.ts
+++ src/declare/auto-import.d.ts
@@ -69,7 +69,6 @@ declare global {
   const triggerRef: typeof import('vue')['triggerRef']
   const unref: typeof import('vue')['unref']
   const useAttrs: typeof import('vue')['useAttrs']
-  const useContextMenu: typeof import('../components/function/MoContextMenu')['useContextMenu']
   const useCssModule: typeof import('vue')['useCssModule']
   const useCssVars: typeof import('vue')['useCssVars']
   const useSlots: typeof import('vue')['useSlots']
